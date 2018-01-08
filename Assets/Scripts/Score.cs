61a437aef364a36a21ea3d143bf5acf308d85a04
matthieu-blin <matth.blin@gmail.com>
2018-01-08 14:36:44 +0100

    score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,20 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class Score : MonoBehaviour
+public class Score : NetworkBehaviour
 {
+    [SyncVar]
 	public int score = 0;					// The player's score.
 
-
-	private PlayerControl playerControl;	// Reference to the player control script.
 	private int previousScore = 0;			// The score in the previous frame.
-
-
-	void Awake ()
-	{
-		// Setting up the reference.
-		playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
-	}
 
 
 	void Update ()
@@ -24,8 +17,16 @@ public class Score : MonoBehaviour
 
 		// If the score has changed...
 		if(previousScore != score)
-			// ... play a taunt.
-			playerControl.StartCoroutine(playerControl.Taunt());
+        {
+
+           var heroes = GameObject.FindGameObjectsWithTag("Player");
+           foreach(var hero in heroes)
+            {
+                var playerControl = hero.GetComponent<PlayerControl>();
+                // ... play a taunt.
+                playerControl.StartCoroutine(playerControl.Taunt());
+            }
+        }
 
 		// Set the previous score to this frame's score.
 		previousScore = score;
