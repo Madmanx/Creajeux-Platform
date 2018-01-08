bd85a3af6e738482c7b844ea8f5ea1ee99c43032
matthieu-blin <matth.blin@gmail.com>
2018-01-08 14:36:54 +0100

    camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -11,14 +11,8 @@ public class CameraFollow : MonoBehaviour
 	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
 
 
-	private Transform player;		// Reference to the player's transform.
+	public Transform player;		// Reference to the player's transform.
 
-
-	void Awake ()
-	{
-		// Setting up the reference.
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-	}
 
 
 	bool CheckXMargin()
@@ -37,6 +31,21 @@ public class CameraFollow : MonoBehaviour
 
 	void FixedUpdate ()
 	{
+        if (player == null)
+        {
+           var heroes = GameObject.FindGameObjectsWithTag("Player");
+           foreach(var hero in heroes)
+            {
+                if (hero.GetComponent<PlayerControl>().isLocalPlayer)
+                {
+                    player = hero.transform;
+                    break;
+                }
+            }
+
+            if (player == null)
+                return;
+        }
 		TrackPlayer();
 	}
 	
